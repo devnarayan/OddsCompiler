d7684b69fcf20b6c26d74ac107157cf00993dc6f
Dev Nagar <devnagar@Live.com>
2015-03-24 15:35:47 +0530

    First Commit

---
+++ OddsData/OddsConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddsData
+{
+   public static class OddsConnection
+    {
+       public static string GetConnectionString()
+       {
+           return "Data Source=111.118.250.186;Initial Catalog=OddsChecker;User ID=sa;Password=Syno2009; Pooling=False;";
+       }
+    }
+}
